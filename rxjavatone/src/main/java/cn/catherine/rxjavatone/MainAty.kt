82d25f69eb317414321cb7ebb2163074d9e20b90
Catherine.Brain William <15520054289@163.com>
2018-04-19 15:02:08 +0800

    建立一个module用于测试Rxjava

---
+++ rxjavatone/src/main/java/cn/catherine/rxjavatone/MainAty.kt
@@ -0,0 +1,12 @@
+package cn.catherine.rxjavatone
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainAty : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.aty_main)
+    }
+}
